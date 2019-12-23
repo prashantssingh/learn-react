291dc82374a4e03b08d59cc895be7f0ea59a2c5a
Prashant Singh <prashant_singh@live.in>
2019-12-22 20:37:31 -0600

    changes in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          {/* Props are good way to pass any captured data (just about anything) to the components.
+          There are no particular tags that needs to used as such to pass these data, hence, flexibility.
+          NOTE: Any data other than STRING, must be wrapped inside parenthesis. 
+          
+          Props provides ways to catch and store data into states for later use through one or more components */}
+          <Header tagline="This is inside props" age={500} cool={true} />
         </div>
-        {/* <Inventory />
-        <Order /> */}
+        <Order />
+        <Inventory />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,19 @@ class Header extends React.Component {
   render () {
     return (
       <header className="top">
-        <h1>Catch of the day</h1>
+        <h1>
+          Catch 
+          <span className="ofThe">
+            <span className="of">Of</span>
+            <span className="the">The</span>
+          </span>
+          Day
+        </h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          {/* Well, WHAT THE HELL?
+          This is effectively saying that for a quick I want to access JavaScript syntax (Object Unwrapping,
+           to be precise) inside the JSX */}
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
