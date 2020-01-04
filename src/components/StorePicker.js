a3e2f827fcb1e6db905b9dca72424897da115a0b
Prashant Singh <prashant_singh@live.in>
2020-01-03 22:44:10 -0800

    add react state handling

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  searchInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text entered in input
+    searchInput = this.searchInput.value.value;
+    // 3. change the page to /store/whatever-enetered
+    this.props.history.push(`/store/{input}`)
+  }
+
   // You must return only one element (however, main element can have any number of element)
   // returning nultiple elements (siblings) in one return is an error in JSX
   // return ----->  <p>An element</p>
@@ -18,12 +29,11 @@ class StorePicker extends React.Component {
   // </React.Fragment>
   // 
   // Also dummy <div> tag works just fine, but convulates the DOM
-
   render() {
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required ref={this.searchInput} placeholder="Store Name"/>
         <button type="submit">Visit Store!</button>
       </form>
     );
