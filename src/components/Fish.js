f9ff6a8435729e0d92a879e9a13d12831e9a55b4
Prashant Singh <prashant_singh@live.in>
2020-01-07 22:23:42 -0600

    update Fish component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import formatPrice from '../helpers'
 
 class Fish extends React.Component {
   render() {
+    const { image, name, price, description, availability } = this.props.details;
     return (
       <li className="menu-fish">
-        <img src={this.props.details.image} alt={this.props.details.name} />
-        <h3 className="fish-name">{this.props.details.name</h3>
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span>{formatPrice(price)}</span>
+        </h3>
+        <p>{description}</p>
+        <button>Add to Cart</button>
       </li>
     );
   }
